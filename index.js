4c68e4a00a0d635b4452df81ada4e1f2ad607827
Avijit Lodh <avijitklodh@gmail.com>
2019-07-11 16:56:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 function dwarfRollCall(dwarves) {
+var dwarfname = "";
+dwarves.forEach(function(name,number){
+  dwarfname += number+1 + ". " + name + " "
+});
+return dwarfname;
 }
 
 function summonCaptainPlanet(planeteerCalls){
+var elements = [];
+  planeteerCalls.forEach(function(elementname) {
+  	elementname = elementname.toUpperCase() + "!";
+  	elements.push(elementname);
+  });
+  return elements;
 }
 
 function longPlaneteerCalls(words) {
+for(var i=0; i<words.length;i++){
+    if (words[i].length>4){
+      return true
+    }
+  }
+ return false
 }
-
 function findTheCheese (foods) {
+  for (var i=0; i<foods.length; i++)
+    if (foods[i]=="cheddar"
+  ||foods[i]=="gouda"
+  ||foods[i]=="camembert"){
+    return foods[i]
+  }
+  return "no cheese!"
 }
